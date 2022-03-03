1499fc3e39d46648095adcaf3bbd8c31ff7a6756
krithikak123 <98815046+krithikak123@users.noreply.github.com>
2022-03-03 03:43:35 -0800

    Delete tomcatDeploy.groovy

--- tomcatDeploy.groovy
+++
@@ -1,8 +0,0 @@
-def call(ip,user,credId){
-  sshagent([credId]) {
-     sh "scp -o StrictHostKeyChecking=no target/*.war ${user}@${ip}:/opt/tomcat8/webapps/app.war"
-     sh "ssh ${user}@${ip} /opt/tomcat8/bin/shutdown.sh"
-     sh "ssh ${user}@${ip} /opt/tomcat8/bin/startup.sh"
-               
-  }
-}
