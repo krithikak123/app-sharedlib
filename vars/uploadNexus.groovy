a3b737e7ffb9f23a5df3fd9249b7f990c4a6adc5
krithikak123 <98815046+krithikak123@users.noreply.github.com>
2022-03-17 22:28:23 +0530

    Update uploadNexus.groovy

--- vars/uploadNexus.groovy
+++ vars/uploadNexus.groovy
@@ -1,4 +1,4 @@
 def call(artiId,credId,grpId,nexusurl,nversion){
 
-     nexusArtifactUploader artifacts: [[artifactId: "${artiId}", file: "target/myweb-${pom.xml}.war", type: 'war']], credentialsId: "${credId}", groupId: "${grpId}", nexusUrl: "${nexusurl}", nexusVersion: "${nversion}", protocol: 'http', repository: 'kk-app-release', version: "${pom.xml}"
+     nexusArtifactUploader artifacts: [[artifactId: "${artiId}", file: "target/myweb-${pomFile.version}.war", type: 'war']], credentialsId: "${credId}", groupId: "${grpId}", nexusUrl: "${nexusurl}", nexusVersion: "${nversion}", protocol: 'http', repository: 'kk-app-release', version: "${pomFile.version}"
 }    
